7606043d44c2caca36569035c93bc186ae6fd7c9
Jay Sitapara <137039486+jay-sitapara@users.noreply.github.com>
2023-09-29 10:11:40 +0530

    Add files via upload

--- 05_assingmenr.sql
+++ 05_assingmenr.sql
@@ -130,6 +130,11 @@ SELECT
 from
     CUSTOMER WHERE STATE='KARNATAKA';
 
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
+--       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
+
 -- 5. Delete the row from the table where PIN CODE is 576201.
 DELETE CUSTOMER WHERE PIN=576201;
 -- 1 row deleted.
@@ -153,6 +158,11 @@ SELECT * FROM CUSTOMER;
 -- 9. List all the records for INVALID (‘I’) persons.
 SELECT * FROM CUSTOMER WHERE STATUS='I';
 
+
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
+
 -- 10. Select all the records with single occurrence (without duplication) of state from the table.
 SELECT DISTINCT(STATE) FROM CUSTOMER; 
 
@@ -165,113 +175,103 @@ SELECT DISTINCT(STATE) FROM CUSTOMER;
 -- 11. Sort and display the customer data, in the alphabetic order of state.
 SELECT * FROM CUSTOMER ORDER BY STATE;  
 
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1004 FELIX                SIMON                M-J-56               PJM      GOA                  403002 12-FEB-71 A
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
+--       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
+--       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
+--       1005 RAJAN                KUTTY                A1 TRADERS           KNR      KERALA               670001 09-JUN-71 A
+
 -- 12. Sort and display the state field in descending order.
-SELECT * FROM CUSTOMER ORDER BY STATE DESC;
+SELECT STATE FROM CUSTOMER ORDER BY STATE DESC;
+
+-- STATE
+-- --------------------
+-- KERALA
+-- KARNATAKA
+-- KARNATAKA
+-- KARNATAKA
+-- GOA
 
 -- 13. Select the records of KARNATAKA customers who are valid ('V').
 SELECT * FROM CUSTOMER WHERE STATE = 'KARNATAKA' AND STATUS = 'V';
--- no rows selected
+
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
 
 -- 14. Retrieve records of Karnataka / Kerala customers.
 SELECT * FROM CUSTOMER WHERE STATE IN ('KARNATAKA', 'KERALA');
 
---    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS
--- ---------- -------------------- -------------------- --------------------
--- CITY     STATE                PIN    B_DATE    S
--- -------- -------------------- ------ --------- -
---       1005 RAJAN                KUTTY                A1 TRADERS
--- KNR      KERALA               670001 09-JUN-71 A
-
---       1006 SHILPA               PAI                  12/4B
--- MNG      KARNATAKA            574154 11-DEC-70 I
-
---       1007 BOSCO                RAKSHIT              R.K. PLAZA
--- BNG      KARNATAKA            576201 01-JAN-71 A
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
+--       1005 RAJAN                KUTTY                A1 TRADERS           KNR      KERALA               670001 09-JUN-71 A
+--       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
+--       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
 
 -- 15. Retrieve records of Karnataka / Kerala customers who are ACTIVE ('A').
 SELECT * FROM CUSTOMER WHERE STATE IN ('KARNATAKA', 'KERALA') AND STATUS = 'A';
---  CUST_NO FIRST_NAME           LAST_NAME            ADDRESS
--- ---------- -------------------- -------------------- --------------------
--- CITY     STATE                PIN    B_DATE    S
--- -------- -------------------- ------ --------- -
---       1005 RAJAN                KUTTY                A1 TRADERS
--- KNR      KERALA               670001 09-JUN-71 A
 
---       1007 BOSCO                RAKSHIT              R.K. PLAZA
--- BNG      KARNATAKA            576201 01-JAN-71 A
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1005 RAJAN                KUTTY                A1 TRADERS           KNR      KERALA               670001 09-JUN-71 A
+--       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
 
 
 -- 16. Retrieve records of Karnataka customers with pin code 576201.
 SELECT * FROM CUSTOMER WHERE STATE = 'KARNATAKA' AND PIN = '576201';
---  CUST_NO FIRST_NAME           LAST_NAME            ADDRESS
--- ---------- -------------------- -------------------- --------------------
--- CITY     STATE                PIN    B_DATE    S
--- -------- -------------------- ------ --------- -
---       1007 BOSCO                RAKSHIT              R.K. PLAZA
--- BNG      KARNATAKA            576201 01-JAN-71 A
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
 
 -- 17. Display all the rows from the table except 1005.
 SELECT * FROM CUSTOMER WHERE CUST_NO != 1005;
---    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS
--- ---------- -------------------- -------------------- --------------------
--- CITY     STATE                PIN    B_DATE    S
--- -------- -------------------- ------ --------- -
---       1004 FELIX                SIMON                M-J-56
--- PJM      GOA                  403002 12-FEB-71 A
 
---       1006 SHILPA               PAI                  12/4B
--- MNG      KARNATAKA            574154 11-DEC-70 I
-
---       1007 BOSCO                RAKSHIT              R.K. PLAZA
--- BNG      KARNATAKA            576201 01-JAN-71 A
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
+--       1004 FELIX                SIMON                M-J-56               PJM      GOA                  403002 12-FEB-71 A
+--       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
+--       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
 
 
 -- 18. Retrieve rows where the state name begins with K and followed by any other character.
 SELECT * FROM CUSTOMER WHERE STATE LIKE 'K%';
 
---    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS
--- ---------- -------------------- -------------------- --------------------
--- CITY     STATE                PIN    B_DATE    S
--- -------- -------------------- ------ --------- -
---       1005 RAJAN                KUTTY                A1 TRADERS
--- KNR      KERALA               670001 09-JUN-71 A
-
---       1006 SHILPA               PAI                  12/4B
--- MNG      KARNATAKA            574154 11-DEC-70 I
-
---       1007 BOSCO                RAKSHIT              R.K. PLAZA
--- BNG      KARNATAKA            576201 01-JAN-71 A
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
+--       1005 RAJAN                KUTTY                A1 TRADERS           KNR      KERALA               670001 09-JUN-71 A
+--       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
+--       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
 
 
 -- 19. Retrieve rows where the name contains the word RAJ embedded in it.
-SELECT * FROM CUSTOMER WHERE (FIRST_NAME || ' ' || LAST_NAME) LIKE '%RAJ%';
+SELECT * FROM CUSTOMER WHERE (FIRST_NAME) LIKE '%RAJ%';
 
--- CUST_NO FIRST_NAME           LAST_NAME            ADDRESS
--- ---------- -------------------- -------------------- --------------------
--- CITY     STATE                PIN    B_DATE    S
--- -------- -------------------- ------ --------- -
---       1005 RAJAN                KUTTY                A1 TRADERS
--- KNR      KERALA               670001 09-JUN-71 A
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
+--       1005 RAJAN                KUTTY                A1 TRADERS           KNR      KERALA               670001 09-JUN-71 A
 
 
 -- 20. Display all the rows whose CUST_NO is from 1003 to 1005.
 SELECT * FROM CUSTOMER WHERE CUST_NO BETWEEN 1003 AND 1005;
---  CUST_NO FIRST_NAME           LAST_NAME            ADDRESS
--- ---------- -------------------- -------------------- --------------------
--- CITY     STATE                PIN    B_DATE    S
--- -------- -------------------- ------ --------- -
---       1004 FELIX                SIMON                M-J-56
--- PJM      GOA                  403002 12-FEB-71 A
-
---       1005 RAJAN                KUTTY                A1 TRADERS
--- KNR      KERALA               670001 09-JUN-71 A
+--    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
+-- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
+--       1004 FELIX                SIMON                M-J-56               PJM      GOA                  403002 12-FEB-71 A
+--       1005 RAJAN                KUTTY                A1 TRADERS           KNR      KERALA               670001 09-JUN-71 A
 
 -- 21. Display all the rows whose dates are in the range of 10-JAN-70 and 31-JUL-96.
-SELECT * FROM CUST where B_DATE between to_date('15-JAN-70') and to_date('17-JUL-96');
+SELECT * FROM CUSTOMER where B_DATE between to_date('15-JAN-70') and to_date('17-JUL-96');
 -- 22. Retrieve all the rows where the city column equals to UDP or MNG or BNG or PGN.
 
 --    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
 -- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
 --       1004 FELIX                SIMON                M-J-56               PJM      GOA                  403002 12-FEB-71 A
 --       1005 RAJAN                KUTTY                A1 TRADERS           KNR      KERALA               670001 09-JUN-71 A
 --       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
@@ -281,6 +281,7 @@ SELECT * FROM CUSTOMER WHERE CITY IN ('UDP', 'MNG', 'BNG', 'PGN');
 
 --    CUST_NO FIRST_NAME           LAST_NAME            ADDRESS              CITY     STATE                PIN    B_DATE    S
 -- ---------- -------------------- -------------------- -------------------- -------- -------------------- ------ --------- -
+--       1003 RAJ                  BAHADUR              SHANTI VILLA         UDP      KARNATAKA            576101 01-AUG-70 V
 --       1006 SHILPA               PAI                  12/4B                MNG      KARNATAKA            574154 11-DEC-70 I
 --       1007 BOSCO                RAKSHIT              R.K. PLAZA           BNG      KARNATAKA            576201 01-JAN-71 A
 
