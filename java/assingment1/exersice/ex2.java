521486d8f04175f4eaa886a392204adbc746f849
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2023-10-06 11:52:00 +0530

    java update

---
+++ java/assingment1/exersice/ex2.java
@@ -0,0 +1,58 @@
+// 2. Create a class called BasicShape that have method display() in which it
+// display message as basic shape and create circle class that extends
+// BasicShape that have method display() that display message as this is circle.
+// create class polygon that extends BasicShape that have method display() that
+// display message as this is polygon. create another class Ellipse that extends
+// Basic Shape that display Message as This is Ellipse. now create object of 3
+// subclass call all methods as well as super class method without creating
+// object of super class.
+
+class BasicShape {
+    void display() {
+        System.out.println("BasicShape Called class");
+    };
+}
+
+class Circle extends BasicShape {
+    void superMethod() {
+        System.out.println("circle Called class");
+        super.display();
+    }
+
+    void display() {
+        System.out.println("circle Called class");
+    };
+}
+
+class Polygon extends BasicShape {
+    void superMethod() {
+        System.out.println("superMethod polygon Called class");
+        super.display();
+    }
+
+    void display() {
+        System.out.println("polygon Called class");
+    };
+}
+
+class Ellipse extends BasicShape {
+    void superMethod() {
+        System.out.println("Ellipse Called class");
+        super.display();
+    }
+
+    void display() {
+        System.out.println("Ellipse Called class");
+    };
+}
+
+public class ex2 {
+public static void main(String[] args) {
+Circle circle1 = new Circle();
+circle1.superMethod();
+Polygon polygon1 = new Polygon();
+polygon1.superMethod();
+Ellipse ellipse1 = new Ellipse();
+ellipse1.superMethod();
+}
+}
