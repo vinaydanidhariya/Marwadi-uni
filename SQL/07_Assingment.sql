750b532744828a0b0e50d96ec78fdd151ebb2dcd
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2023-10-12 15:04:35 +0530

    "sql assingment 7"

--- SQL/07_Assingment.sql
+++ SQL/07_Assingment.sql
@@ -1,9 +1,12 @@
-
 -- -- * ASSIGNMENT – 7 *
 --  Create the Following Tables:
-
 -- 1. Create LOCATION Table with columns Location_Id, Regional_Group.
 -- Constraints on LOCATION table: Location_Id Primary Key.
+CREATE TABLE LOCATION(
+    LOCATION_ID NUMBER(3) PRIMARY KEY,
+    REGIONAL_GROUP VARCHAR2(10)
+);
+
 -- 2. Insert the following records into the table LOCATION:
 -- LOCATION_ID REGIONAL_GROUP
 -- ------------- ------------------------------
@@ -11,10 +14,36 @@
 -- 123 DALLAS
 -- 124 CHICAGO
 -- 167 BOSTON
+INSERT INTO
+    LOCATION
+VALUES
+    (122, 'NEW YORK');
+
+INSERT INTO
+    LOCATION
+VALUES
+    (123, 'DALLAS');
+
+INSERT INTO
+    LOCATION
+VALUES
+    (124, 'CHICAGO');
+
+INSERT INTO
+    LOCATION
+VALUES
+    (167, 'BOSTON');
+
 -- 3. Create DEPARTMENT Table with columns Department_Id, Name,
 -- Location_ID.
 -- Constraints on DEPARTMENT table: Department_Id Primary Key, Location_Id
 -- references LOCATION table.
+CREATE TABLE DEPARTMENT(
+    DEPARTMENT_ID NUMBER(2) PRIMARY KEY,
+    NAME VARCHAR2(10),
+    LOCATION_ID NUMBER(3) REFERENCES LOCATION(LOCATION_ID)
+);
+
 -- 4. Insert the following records into DEPARTMENT table:
 -- DEPRATMENT_ID NAME LOCATION_ID
 -- ------------------------ --------------------- ---------------------
@@ -22,8 +51,33 @@
 -- 20 RESEARCH 124
 -- 30 SALES 123
 -- 40 OPERATIONS 167
+INSERT INTO
+    DEPARTMENT
+VALUES
+    (10, 'ACCOUNTING', 122);
+
+INSERT INTO
+    DEPARTMENT
+VALUES
+    (20, 'RESEARCH', 124);
+
+INSERT INTO
+    DEPARTMENT
+VALUES
+    (30, 'SALES', 123);
+
+INSERT INTO
+    DEPARTMENT
+VALUES
+    (40, 'OPERATIONS', 167);
+
 -- 5. Create JOB Table with columns Job_Id, Funcation.
 -- Constraints on JOB table: Job_ID Primary Key.
+CREATE TABLE JOB(
+    JOB_ID NUMBER(3) PRIMARY KEY,
+    FUNCTION VARCHAR2(10)
+);
+
 -- 6. Insert the following records into JOB table:
 -- JOB_ID FUNCTION
 -- ------------------ -------------------
@@ -33,15 +87,53 @@
 -- 670 SALESPERSON
 -- 671 MANAGER
 -- 672 PRESIDENT
--- M.U. M.C.A.
--- Semester - I
--- 05MC0105 – RDBMS
--- * ASSIGNMENT – 7 *
+INSERT INTO
+    JOB
+VALUES
+    (667, 'CLERK');
+
+INSERT INTO
+    JOB
+VALUES
+    (668, 'STAFF');
+
+INSERT INTO
+    JOB
+VALUES
+    (669, 'ANALYST');
+
+INSERT INTO
+    JOB
+VALUES
+    (670, 'SALESPERSON');
+
+INSERT INTO
+    JOB
+VALUES
+    (671, 'MANAGER');
+
+INSERT INTO
+    JOB
+VALUES
+    (672, 'PRESIDENT');
+
 -- Create EMPLOYEE Table with columns Employee_Id, Last_Name, First_Name,
 -- Middle_Name, Job_Id, Manager_Id, Hire_Date, Salary, Comm, Department_ID.
 -- Constraints on EMPLOYEE table: Employee_Id Primary Key, Last_Name
 -- NotNull, Department_Id references DEPARTMENT table,Job_id references JOB
--- TABLE
+-- TABLE.
+CREATE TABLE EMPLOYEE (
+    EMPLOYEE_ID NUMBER(4) PRIMARY KEY,
+    LAST_NAME VARCHAR2(10) NOT NULL,
+    FIRST_NAME VARCHAR2(10),
+    MIDDLE_NAME CHAR(1),
+    JOB_ID NUMBER(3) REFERENCES JOB(JOB_ID),
+    MANAGER_ID NUMBER(4),
+    HIRE_DATE DATE,
+    SALARY NUMBER(4),
+    COMM NUMBER(3),
+    DEPARTMENT_ID NUMBER(20) REFERENCES DEPARTMENT(DEPARTMENT_ID)
+);
 
 -- 8. Insert the following records into EMPLOYEE table:
 -- EMPLOYEE_ID LAST_NAME FIRST_NAME MIDDLE_NAME JOB_ID MANAGER_ID   HIRE_DATE SALARY    COMM   DEPARTMENT_ID
@@ -52,6 +144,101 @@
 -- 7506         DENNIS    LYNN          S        671    7839        15-MAY-85   2750    NULL   30
 -- 7507         BAKER     LESLIE        D        671    7839        10-JUN-85   2200    NULL   40
 -- 7521         WARK      CYNTHIA       D        670    7698        22-FEB-85   1250    500    30
+INSERT INTO
+    EMPLOYEE
+VALUES
+    (
+        7369,
+        'SMITH',
+        'JOHN',
+        'Q',
+        667,
+        7902,
+        '17-DEC-84',
+        800,
+        NULL,
+        20
+    );
+
+INSERT INTO
+    EMPLOYEE
+VALUES
+    (
+        7499,
+        'ALLEN',
+        'KEVIN',
+        'J',
+        670,
+        7698,
+        '20-FEB-85',
+        1600,
+        300,
+        30
+    );
+
+INSERT INTO
+    EMPLOYEE
+VALUES
+    (
+        7505,
+        'DOYLE',
+        'JEAN',
+        'K',
+        671,
+        7839,
+        '04-APR-85',
+        2850,
+        NULL,
+        30
+    );
+
+INSERT INTO
+    EMPLOYEE
+VALUES
+    (
+        7506,
+        'DENNIS',
+        'LYNN',
+        'S',
+        671 7839,
+        '15-MAY-85',
+        2750,
+        NULL,
+        30
+    );
+
+INSERT INTO
+    EMPLOYEE
+VALUES
+    (
+        7507,
+        'BAKER',
+        'LESLIE',
+        'D',
+        671,
+        7839,
+        '10-JUN-85',
+        2200,
+        NULL,
+        40
+    );
+
+INSERT INTO
+    EMPLOYEE
+VALUES
+    (
+        7521,
+        'WARK',
+        'CYNTHIA',
+        'D',
+        670,
+        7698,
+        '22-FEB-85',
+        1250,
+        500,
+        30
+    );
+
 --  Perform the following queries on the above given table:
 -- 1. List all job details.
 -- 2. List all the locations.
