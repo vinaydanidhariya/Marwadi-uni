9b7f39147bdd17901325f246d87a48a1cdb110e7
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2023-11-09 12:38:29 +0530

    ""

--- java/assingment1/string assingment/p1.java
+++ java/assingment1/string assingment/p1.java
@@ -68,37 +68,104 @@ public class p1 {
     // Input : Hello ! How are you ?
     // Character to find : H
     // Output : 2
+    /**
+     * 
+     */
     void charCount() {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter string");
-        String word = sc.nextLine();
+        String someString = sc.nextLine();
         System.out.println("Enter find char in string");
-        char char = sc.nextChar();
+        char someChar = sc.nextLine().charAt(0);
+        int charCount = 0;
+        for (int i = 0; i < someString.length(); i++) {
+            if (someString.charAt(i) == someChar) {
+                charCount++;
+            }
+        }
+        System.out.println(someChar + " COUNT OF CHAR " + charCount);
+    }
 
-        word.charAt(i);
+    // (6) Write a java program to enter two strings and prepare a new string by
+    // combining both the
+    // strings in following manner :
+    // Input String one : abc
+    // Input string two : xyz
+    // output : axbycz
+    void combiningString() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First Enter string");
+        String firstString = sc.nextLine();
+        System.out.println("Second Enter string");
+        String secondString = sc.nextLine();
+        String newString = "";
+        if (firstString.length() == secondString.length()) {
+            for (int i = 0; i < firstString.length(); i++) {
+                newString += firstString.charAt(i) + "" + secondString.charAt(i);
+            }
+            System.out.println("Combine String : " + newString);
+        } else {
+            System.out.println("NOT SAME LENGTH STRING");
+        }
+    }
+
+    // (7) Write a java program to perform following operation :
+    // Input String : Hello
+    // Input a character : X
+    // Input number of times : 3
+    // Output : HelloXHelloXHello
+    void multiString() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String someString = sc.nextLine();
+        System.out.println("Enter character");
+        char someChar = sc.next().charAt(0);
+        String newString = "";
+        for (int i = 0; i < 3; i++) {
+            newString += someString + "" + someChar;
+        }
+        System.out.println(newString);
+
+    }
+
+    // (8) Write a java program to enter a string from keyboard and count number of
+    // Vowels and No. of
+    // consonants. Also give percentage of both.
+    void countVowel() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String someString = sc.nextLine();
+        int vowelCount = 0;
+        int constantCount = 0;
+        for (int i = 0; i < someString.length(); i++) {
+            if (someString.charAt(i) == 'A'
+                    || someString.charAt(i) == 'a'
+                    || someString.charAt(i) == 'E'
+                    || someString.charAt(i) == 'e'
+                    || someString.charAt(i) == 'I'
+                    || someString.charAt(i) == 'i'
+                    || someString.charAt(i) == 'O'
+                    || someString.charAt(i) == 'o'
+                    || someString.charAt(i) == 'U'
+                    || someString.charAt(i) == 'u') {
+                vowelCount++;
+            } else if (someString.charAt(i) == " ") {
+                
+            }
+            else{
+                constantCount++;
+            }
+        }
+        System.out.println("NUMBER OF VOWELS : " + vowelCount);
+        System.out.println("NUMBER OF CONSTANT : " + constantCount);
     }
 
     public static void main(String[] args) {
         p1 p = new p1();
-        p.doubleChar();
+        p.countVowel();
     }
 }
 
-// (6) Write a java program to enter two strings and prepare a new string by
-// combining both the
-// strings in following manner :
-// Input String one : abc
-// Input string two : xyz
-// output : axbycz
-// (7) Write a java program to perform following operation :
-// Input String : Hello
-// Input a character : X
-// Input number of times : 3
-// Output : HelloXHelloXHello
-// (8) Write a java program to enter a string from keyboard and count number of
-// Vowels and No. of
-// consonants. Also give percentage of both.
 // (9) Write a java program to enter a string, character to replace and
 // replacement character. Output
 // will be new string.
