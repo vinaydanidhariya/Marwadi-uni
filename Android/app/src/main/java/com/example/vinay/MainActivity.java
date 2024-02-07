8db54d4d62d312a54d7f8f1d5205b469f587519d
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2024-02-07 15:25:05 +0530

    login screen add

--- Android/app/src/main/java/com/example/vinay/MainActivity.java
+++ Android/app/src/main/java/com/example/vinay/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.vinay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt_view;
+    Button Btn_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_view = findViewById(R.id.txt_box);
+        Btn_click = findViewById(R.id.btn_click);
+
+        Btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_view.setText("RAM");
+            }
+        });
     }
 }
