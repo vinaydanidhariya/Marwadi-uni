5741f91839247a6961c540bafb33423cca4a52e0
Danidhariya Vinay <66329591+vinaydanidhariya@users.noreply.github.com>
2024-02-07 10:44:05 +0000

    "new changes"

--- PYTHON/UNIT 1/3.py
+++ PYTHON/UNIT 1/3.py
@@ -5,13 +5,23 @@
 # grade
 
 
-sub1= int(input("Enter subject 1 Mark :"))
-sub2= int(input("Enter subject 2 Mark :"))
-sub3= int(input("Enter subject 3 Mark :"))
-sub4= int(input("Enter subject 4 Mark :"))
+sub1= float(input("Enter subject 1 Mark :"))
+sub2= float(input("Enter subject 2 Mark :"))
+sub3= float(input("Enter subject 3 Mark :"))
+sub4= float(input("Enter subject 4 Mark :"))
 
 total = sub1+ sub2 + sub3 + sub4
 percentage = total/4
 grade = 0 
-print(total)
-
+print("Total : " , total)
+print("Percentage : " ,percentage)
+if percentage > 90 and percentage < 100:
+    print("Grade A+")
+elif percentage > 80 and percentage < 90:
+    print("Grade A")
+elif(percentage > 70 and percentage < 80):
+    print("Grade B")
+elif(percentage < 60 and percentage < 70 ):
+    print("Grade C")
+else:
+    print("000000000000000000000000")
---
+++ PYTHON/UNIT 1/5.py
@@ -0,0 +1,13 @@
+
+
+lst=[] 
+num= 0
+for i in range(10):
+    lst[i] = int(input("ENTER NUMBER : "))
+    if(i%2==0):
+        if(num<i):
+            num=i
+
+print(num)
+
+
