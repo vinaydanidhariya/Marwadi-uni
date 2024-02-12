f5d809a81484eb446636d0e9e3d8a4d0e61832f1
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2024-02-12 15:32:07 +0530

    dasdasda

--- Android/app/src/main/java/com/example/vinay/MainActivity.java
+++ Android/app/src/main/java/com/example/vinay/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 txt_view.setText("RAM");
+                Toast toast=Toast.makeText(getApplicationContext(),"RAM",Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
     }
