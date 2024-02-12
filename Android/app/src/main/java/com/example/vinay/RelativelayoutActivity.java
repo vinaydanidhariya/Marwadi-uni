0cb7d28c167664a7101ec05ad49abde33c25dcbf
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2024-02-12 15:31:59 +0530

    adsa

---
+++ Android/app/src/main/java/com/example/vinay/RelativelayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.vinay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativelayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+    }
+}
