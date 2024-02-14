4033bafd5f3a35e374a3323fe28c3d5fbe1da4c6
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2024-02-14 11:29:14 +0530

    "menu devien list"

--- PYTHON/UNIT 1/4.py
+++ PYTHON/UNIT 1/4.py
@@ -1,51 +1,38 @@
-arr = []
+def chk_armstrong(num):
+    sum = 0
+    for i in num:
+        sum += int(i) ** len(num)
+    return sum == int(num)
 
-for i in range(10):
-    arr.append(input(f"ENTER NUMBER {i+1} : "))
-
+arr = [input(f"ENTER NUMBER {i+1} : ") for i in range(10)]
 print(arr)
 
-def chk_armstrong(num):
-    sum =0
-    for i in num:
-        sum = sum +pow(int(i),len(num))
-    if(int(sum) == int(num)):
-        return True
-    else: 
-        return False
-    
-
-
-num1 =chk_armstrong('153')
-print(num1) 
 for i in arr:
-    ifnum = chk_armstrong(i) 
-    if(ifnum == True):
-        print("NUMBER IS ARMSTRONG ",i)
+    if chk_armstrong(i):
+        print("NUMBER IS ARMSTRONG", i)
     else:
-        print("NUMBER NOT ARMSTRONG ",i)
+        print("NUMBER NOT ARMSTRONG", i)
 
 
 
 # ENTER NUMBER 1 : 153
-# ENTER NUMBER 2 : 156
-# ENTER NUMBER 3 : 456
-# ENTER NUMBER 4 : 1634
-# ENTER NUMBER 5 : 265
-# ENTER NUMBER 6 : 11
-# ENTER NUMBER 7 : 46854
-# ENTER NUMBER 8 : 45
-# ENTER NUMBER 9 : 352
-# ENTER NUMBER 10 : 255
-# ['153', '156', '456', '1634', '265', '11', '46854', '45', '352', '255']
-# True
-# NUMBER IS ARMSTRONG  153
-# NUMBER NOT ARMSTRONG  156
-# NUMBER NOT ARMSTRONG  456
-# NUMBER IS ARMSTRONG  1634
-# NUMBER NOT ARMSTRONG  265
-# NUMBER NOT ARMSTRONG  11
-# NUMBER NOT ARMSTRONG  46854
-# NUMBER NOT ARMSTRONG  45
-# NUMBER NOT ARMSTRONG  352
-# NUMBER NOT ARMSTRONG  255
+# ENTER NUMBER 2 : 154
+# ENTER NUMBER 3 : 123
+# ENTER NUMBER 4 : 121
+# ENTER NUMBER 5 : 152
+# ENTER NUMBER 6 : 1456
+# ENTER NUMBER 7 : 144
+# ENTER NUMBER 8 : 123
+# ENTER NUMBER 9 : 366
+# ENTER NUMBER 10 : 58
+# ['153', '154', '123', '121', '152', '1456', '144', '123', '366', '58']
+# NUMBER IS ARMSTRONG 153
+# NUMBER NOT ARMSTRONG 154
+# NUMBER NOT ARMSTRONG 123
+# NUMBER NOT ARMSTRONG 121
+# NUMBER NOT ARMSTRONG 152
+# NUMBER NOT ARMSTRONG 1456
+# NUMBER NOT ARMSTRONG 144
+# NUMBER NOT ARMSTRONG 123
+# NUMBER NOT ARMSTRONG 366
+# NUMBER NOT ARMSTRONG 58
