8b735b8c99736405c6faa6a8e2565e467bd281fe
Danidhariya Vinay <66329591+vinaydanidhariya@users.noreply.github.com>
2024-02-15 16:09:28 +0530

    Update 8.py

--- PYTHON/UNIT 1/8.py
+++ PYTHON/UNIT 1/8.py
@@ -5,16 +5,32 @@
 # palindrom or not
 
 str1 = input("ENTER STRING :- ")
+LENGTH = 0
+count=0
+for i in str1:
+    LENGTH += 1
+    if(i in 'aeiou'):
+        count += 1
 
-def count_vowel(str1):
-    count=0
-    for i in str1:
-        print(i)
-        if(i == 'a' or i == 'i' or i == 'e' or i ==  'o' or i ==  'u'):
-            count= count + 1
-    print(count)
-    
-count_vowel(str1)
+print("COUNT OF VOWEL ",count)
+print("LENGTH OF STRING : ",LENGTH)  
+# revers string
+reversedstr = str1[::-1]
+print("REVERSED STRING : ",reversedstr)
+# find and repace
+print(str1.replace('a','*'))
+# palindrom of not
+if(str1==reversedstr):
+    print("PALINDROM")
+else:
+    print("NOT PALINDROM")
 
-print(len(str1))
-print(str1)
+
+# ENTER STRING :- vinay
+# COUNT OF VOWEL  2
+# LENGTH OF STRING :  5
+# REVERSED STRING :  yaniv
+# vin*y
+# NOT PALINDROM
+
+
