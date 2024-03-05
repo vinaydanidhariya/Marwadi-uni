cc78a8463ccdc223a337b332f9b161c2b797e530
DhruvKanjiya <153075428+DhruvKanjiya@users.noreply.github.com>
2024-03-05 20:44:50 +0530

    Update unit2.py
    fefef

--- DAV/unit2.py
+++ DAV/unit2.py
@@ -1,1 +1,12 @@
+#PRG1.Write a program to create a list of names; then define a function to display all the elements in the recevied list.
+#Call the fuction to execute its statement and display all name in the list. 
 
+def display_names(name_list):
+    for name in name_list:
+        print(name)
+
+# Create a list of names
+names = ["Viny", "Rahul", "Dhruv", "Jay", "Vraj"]
+
+# Call the function to display all names in the list
+display_names(names)
