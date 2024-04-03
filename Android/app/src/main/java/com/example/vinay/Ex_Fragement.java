660ce123331a149deb1e5e19b9d8d49c4e693900
Vinay danidhariya <vinay.danidhariya121369@marwadiuniversity.ac.in>
2024-04-03 14:42:31 +0530

    Add fragment classes and update layout files

--- Android/app/src/main/java/com/example/vinay/Ex_Fragement.java
+++ Android/app/src/main/java/com/example/vinay/Ex_Fragement.java
@@ -1,16 +1,38 @@
 package com.example.vinay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.vinay.fragment.One;
+import com.example.vinay.fragment.Two;
 
 public class Ex_Fragement extends AppCompatActivity {
-
+    Button homeBtn,aboutBtn;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex_fragement);
 
+    homeBtn=findViewById(R.id.f_btn_home);
+    aboutBtn=findViewById(R.id.f_btn_about);
 
+    homeBtn.setOnClickListener(view -> {
+        fragment = new One();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.home_fragment,fragment).commit();
+    });
+    aboutBtn.setOnClickListener(view -> {
+        fragment = new Two();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.home_fragment,fragment).commit();
+    });
     }
 }
--- PYTHON/UNIT2/10.py
+++ PYTHON/UNIT2/10.py
@@ -1,18 +1,27 @@
 # write a prg to zip and unzip files using python
+import os
 
+# get the current working directory
+current_working_directory = os.getcwd()
+
+# print output to the console
+print(current_working_directory)
+
+# PYTHON\UNIT2
 import zipfile
 import os 
 def zip_files(file_name):
     with zipfile.ZipFile(file_name, 'w') as zip:
-        path = os.path.basename("PYTHON\UNIT 2\file1.txt")
-        print(path)
-        zip.write('text.txt')
+        for folder, subfolders, files in os.walk(os.getcwd()):
+            for file in files:
+                zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder, file), os.getcwd()), compress_type=zipfile.ZIP_DEFLATED)
         print('Files zipped successfully')
+        
         
 def unzip_files(file_name):
     with zipfile.ZipFile(file_name, 'r') as zip:
         zip.extractall()
         print('Files unzipped successfully')
         
-zip_files('files.zip')
+zip_files('filessas.zip')
 unzip_files('files.zip')
