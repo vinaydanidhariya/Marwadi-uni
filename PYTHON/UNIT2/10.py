2b7ce60482d59777988f011f900407f784653c33
vinaydanidhariya <vinaydanidhariya4114@gmail.com>
2024-04-08 16:04:48 +0530

    prg 10

--- PYTHON/UNIT2/10.py
+++ PYTHON/UNIT2/10.py
@@ -1,15 +1,5 @@
 # write a prg to zip and unzip files using python
-import os
-
-# get the current working directory
-current_working_directory = os.getcwd()
-
-# print output to the console
-print(current_working_directory)
-
-# PYTHON\UNIT2
 import zipfile
-import os 
 def zip_files(file_name):
     with zipfile.ZipFile(file_name, 'w') as zip:
         zip.write('students.csv')
@@ -25,5 +15,5 @@ zip_files('files.zip')
 unzip_files('files.zip')
 # Files zipped successfully
 # File Name                                             Modified             Size
-# students.csv                                   2024-04-08 16:01:02           24
+# students.csv                                   2024-04-08 16:01:50           24
 # Files unzipped successfully
