ff2bbf03ca14e78e321073a06e061261f6f471a6
DhruvKanjiya <153075428+DhruvKanjiya@users.noreply.github.com>
2024-05-03 10:55:16 +0530

    Update 3.py

--- PYTHON/UNIT4/3.py
+++ PYTHON/UNIT4/3.py
@@ -1,16 +1,16 @@
 import pandas as pd
-
+#dropna
 # Load the Excel file
 df = pd.read_excel('student_records.xlsx')
 
-#newdf = df.dropna()
-#print(newdf)
+newdf = df.dropna()
+print(newdf)
 
 #fillna
 
 df = pd.read_excel('student_records.xlsx')
-newdf=df.fillna(1111)
-print(newdf)
+newf=df.fillna(1111)
+print(newf)
 #output
 #       RollNo    Name   Gender                 E-Mail     MobileNo.     Age               City
 # 20     121      Dhruv   1111                   1111     1.111000e+03     1111.0               1111
